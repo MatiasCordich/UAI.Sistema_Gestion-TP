42cfd948c8513af4da2b6b731e789dbaa8b848b3
MatiasCordich <m.scordich@gmail.com>
2026-04-11 22:09:47 -0300

    Se agregan las entidades de las tablas intermedias.

---
+++ ENTITY/AlumnoMateria.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY
+{
+    /* Clase AlumnoMateria - Representa la inscripción de un alumno en una materia */
+    public class AlumnoMateria
+    {
+        // Columnas de la tabla 
+        public int IdAlumno { get; set; }
+        public int IdMateria { get; set; }
+        public int IdProfesor { get; set; }
+
+        // Objetos completo 
+        public Materia Materia { get; set; }
+        public Alumno AlumnoInscripto { get; set; }
+        public Profesor ProfesorAsignado { get; set; }
+
+        // Propiedades calculadas
+        public string ResumenInscripcion => $"{Materia?.Nombre ?? "Sin materia"} - Prof. {ProfesorAsignado?.NombreCompleto ?? "Sin asignar"}";
+    }
+}
---
+++ ENTITY/ProfesorMateria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY
+{
+    /* Clase ProfesorMateria - Representa las asignaciones de profesores a una materia en el sistema */
+    public class ProfesorMateria
+    {
+        // Columnas de la tabla (IDs planos para DAL)
+        public int IdProfesor { get; set; }
+        public int IdMateria { get; set; }
+
+        // Objetos completos
+        public Profesor Profesor { get; set; }
+        public Materia Materia { get; set; }
+
+        // Propiedades calculadas
+        public string ResumenAsignacion => $"{Profesor?.NombreCompleto ?? "Sin profesor"} - {Materia?.Nombre ?? "Sin materia"}";
+    }
+}
